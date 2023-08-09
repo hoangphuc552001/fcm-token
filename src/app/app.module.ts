9e7ac43261398ff7c5577ffe64dc37d75a6debe4
hoangphuc552001 <hoangphuc552001@gmail.com>
2023-08-09 23:39:46 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireMessagingModule} from "@angular/fire/compat/messaging";
+import {AngularFireModule} from "@angular/fire/compat";
+import {MessagingService} from "./service/messaging-service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBpk2M7YGRvX-S0DxUds_Oxhq-Fv3SZPLM",
+      authDomain: "udpt-2023.firebaseapp.com",
+      projectId: "udpt-2023",
+      storageBucket: "udpt-2023.appspot.com",
+      messagingSenderId: "958350586161",
+      appId: "1:958350586161:web:b736f4e993798649094010",
+      measurementId: "G-GKVZFFQGVJ"
+    })
   ],
-  providers: [],
+  providers: [MessagingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/service/messaging-service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {AngularFireMessaging} from "@angular/fire/compat/messaging";
+
+@Injectable()
+export class MessagingService {
+  currentMessage = new BehaviorSubject<any>(null);
+
+  constructor(private angularFireMessaging: AngularFireMessaging) {
+  }
+
+  requestPermission() {
+    this.angularFireMessaging.requestToken.subscribe(
+      (token) => {
+        console.log(token);
+      },
+      (err) => {
+        console.error('Unable to get permission to notify.', err);
+      }
+    );
+  }
+
+  receiveMessage() {
+    this.angularFireMessaging.messages.subscribe(
+      (payload: any) => {
+        console.log('new message received. ', payload);
+        this.currentMessage.next(payload);
+      })
+  }
+}
